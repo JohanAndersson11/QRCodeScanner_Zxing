e282a08cf68d83868db3a94b73291446af74348e
JohanAndersson11 <Scott.atta@hotmail.com>
2017-01-26 19:20:30 +0100

    QRCodeScan
    This code is Android QR Code Scanner example using Zxing Library.

---
+++ app/src/main/java/com/pcb/mobile/qrcodescanner/MainActivity.java
@@ -0,0 +1,71 @@
+package com.pcb.mobile.qrcodescanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button scannerBtn;
+    private TextView viewName, viewAddress;
+
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scannerBtn = (Button) findViewById(R.id.buttonScan);
+
+        viewName = (TextView) findViewById(R.id.textViewName);
+        viewAddress = (TextView) findViewById(R.id.textViewAddress);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        scannerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                qrScan.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    viewName.setText(obj.getString("name"));
+                    viewAddress.setText(obj.getString("address"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
